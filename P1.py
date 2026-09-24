29d49706a52b66e874c328f0b01e5355f617d294
JuanBonapelch <juanbona2018@gmail.com>
2026-09-24 09:59:05 -0300

    hola

--- P1.py
+++ P1.py
@@ -8,4 +8,12 @@ df = pd.DataFrame(archivo)
 df = df[["salario_mensual_o_retiro_neto_en_tu_moneda_local","trabajo_de","anos_de_experiencia","la_recomendas_como_un_buen_lugar_para_trabajar",
     "estado","carrera"]]
 
-print(df)
+df = df.rename(columns = {"salario_mensual_o_retiro_neto_en_tu_moneda_local": "salario mensual neto", "trabajo_de": "puesto",
+                          "la_recomendas_como_un_buen_lugar_para_trabajar": "percepcion condiciones de trabajo","estado": "estado academico"})
+
+salarios = df["salario mensual neto"]
+
+media = salarios.mean()
+print(media)
+print(f"maximo: {salarios.max()}")
+print(f"minimi: {salarios.min()}")
